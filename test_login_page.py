397490c69aea2716a8a9b16abc9dcc2f68dbbffb
Tatiana <perm_tatiana@mail.ru>
2022-12-21 22:09:50 +0300

    test_login_page.py changed. 3 checks combined into 1 test case

--- test_login_page.py
+++ test_login_page.py
@@ -1,20 +1,30 @@
 from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
+from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
 
-def test_guest_should_see_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_register_form()
+#def test_guest_should_see_register_form(browser):
+    #link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    #page = LoginPage(browser, link)
+    #page.open()
+    #page.should_be_register_form()
 
-def test_guest_should_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_form()
+#def test_guest_should_see_login_form(browser):
+    #link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    #page = LoginPage(browser, link)
+    #page.open()
+    #page.should_be_login_form()
 
-def test_guest_should_be_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
+#def test_guest_should_be_login_url(browser):
+    #link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    #page = LoginPage(browser, link)
+    #page.open()
+    #page.should_be_login_url()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_url()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
