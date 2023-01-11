c00a7da52f603ec2ce6584cc0c9b059f83841420
Tatiana <perm_tatiana@mail.ru>
2023-01-11 19:40:06 +0300

    added 2 files  - 'product_page.py', 'test_product_page.py'. There are 10 test-cases (1 - xfailed)

--- pages/locators.py
+++ pages/locators.py
@@ -7,5 +7,16 @@ class MainPageLocators():
 class LoginPageLocators():
     register_form = (By.CSS_SELECTOR, '#register_form')
     login_form = (By.CSS_SELECTOR, "#login_form")
+
+class ProductPageLocators():
+    button_add_into_basket = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    find_product_name = (By.CSS_SELECTOR, ".product_main h1")
+    find_product_price = (By.CSS_SELECTOR, ".product_main p")
+    basket_find_product_name = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) strong")
+    basket_find_product_price = (By.CSS_SELECTOR, "#messages .alert:nth-child(3) strong")
+
+
+
+
     
 
--- test_login_page.py
+++ test_login_page.py
@@ -1,6 +1,5 @@
 from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
-from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 #def test_guest_should_see_register_form(browser):
