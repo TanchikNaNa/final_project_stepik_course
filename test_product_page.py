c6836d40e3035f968f9bdf04baaeaf2c271ae018
Tatiana <perm_tatiana@mail.ru>
2023-01-27 22:14:13 +0300

    changed test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -15,7 +15,6 @@ import time
                                    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks =pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
